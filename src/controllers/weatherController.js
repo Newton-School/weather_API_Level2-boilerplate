578edb7be2988bd495e845a96901ae695da9a174
prakharmishra-cyber <mprakhar35@gmail.com>
2023-06-20 14:41:11 +0530

    done

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -25,11 +25,25 @@ async function saveDataToDatabase(data) {
   });
 }
 
+/*
+  Instructions for students:
+  Implement the function getForecastDataByName(cityName) that retrieves the 7-day weather forecast data for a city by its name.
+
+  Tips:
+    - Use the getDataFromDatabase() function to retrieve the data from the database.
+    - Access the array of cities and their forecast data from the returned result.
+    - Find the city object with the matching name and extract its forecast data.
+    - Return the appropriate JSON response based on the result.
+
+*/
+
 
 // Level 2: Get 7 Days Weather Forecast Data by Name
 async function getForecastDataByName(cityName) {
    // TODO: Implement this function
 }
+
+
 
 
 module.exports = {
--- src/routes/weatherRoutes.js
+++ src/routes/weatherRoutes.js
@@ -2,7 +2,37 @@ const express = require('express');
 const router = express.Router();
 const weatherController = require('../controllers/weatherController');
 
+/*
+  Instructions for students:
+  Implement the route handler for retrieving the 7-day weather forecast data for a city by its name.
 
+  Route:
+    GET /forecast/city/:name
+
+  Input:
+    - :name (string): The name of the city for which forecast data needs to be retrieved.
+
+  Output:
+    - If the city name is found in the database, send a JSON response with the following structure and a status code of 200:
+      {
+        "status": "success",
+        "message": "Forecast data retrieved",
+        "data": { ... } // Forecast data object for the specified city
+      }
+
+    - If the city name is not found in the database, send a JSON response with the following structure and a status code of 404:
+      {
+        "status": "error",
+        "error": "City not found",
+        "message": "Failed to retrieve forecast data"
+      }
+      
+  Tips:
+    - Use the weatherController.getForecastDataByName() function to retrieve the forecast data for the specified city.
+    - Pass the city name from the request parameters to the getForecastDataByName() function.
+    - Handle the returned result and send the appropriate JSON response based on the result.
+
+*/
 
 // Level 2: Get 7 Days Weather Forecast Data by Name
 router.get('/forecast/city/:name', async (req, res) => {
